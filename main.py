19226fb6248334497cf59adb9d6e1a403aebeafc
Khimich13 <dober.man616@gmail.com>
2025-09-15 21:53:52 -0400

    created asteroid, asteroidfield, circleshape, player, shot files/classes. Updated constants and main. I had to do much more commits.

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game over!")
+                return
+            for bullet in shots:
+                if asteroid.collision_check(bullet):
+                    asteroid.split()
+                    bullet.kill()
         screen.fill(color="black")
+        for unit in drawable:
+            unit.draw(screen)
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000.0  # Convert milliseconds to seconds
         
-    print(f"Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
